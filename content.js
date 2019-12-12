4f5a75aec156aae8ce2b5cf0081fa9ec53b4535b
Brad Smith <brad@smithwebtek.com>
2019-12-12 13:58:44 -0500

    Basic bear count added to html response

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
-chrome.runtime.onMessage.addListener(function (request) {
-  const regexp = new RegExp('bear', 'gi')
-  let matches = document.documentElement.innerHTML.match(regexp)
-
-  matches = matches ? matches : "ZERO"
-
-  alert(`We found  ${matches.length} bears on this page.`)
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const rxp = new RegExp('bear', 'gi')
+  let matches = document.documentElement.innerHTML.match(rxp)
+  const matchesCount = () => {
+    if (matches) {
+      return matches.length
+    } else {
+      return "ZERO"
+    }
+  }
+  sendResponse({
+    count: matchesCount()
+  })
 });
--- popup.js
+++ popup.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('button').addEventListener('click',
-    whenClicked, false)
+    onclick, false)
 
-    function whenClicked() {
+    function onclick() {
       chrome.tabs.query({ currentWindow: true, active: true },
         function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, "Hi")
+          chrome.tabs.sendMessage(tabs[0].id, "Hi", setCount)
         }
       )
     }
-})
+
+  function setCount(response) {
+    const div = document.createElement('div')
+    div.textContent = `${response.count ? response.count : 'ZERO'} bears found`
+    document.body.appendChild(div)
+  }
+}, false)
