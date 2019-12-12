9a3020389085a41f998a42f33d6fae9a58336989
Brad Smith <brad@smithwebtek.com>
2019-12-12 14:42:57 -0500

    Completed bear tutorial

--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click',
-    onclick, false)
 
-    function onclick() {
-      chrome.tabs.query({ currentWindow: true, active: true },
-        function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, "Hi", setCount)
-        }
-      )
-    }
-
-  function setCount(response) {
+  const bg = chrome.extension.getBackgroundPage()
+  Object.keys(bg.bears).forEach(function(url) {
+    console.log(url)
     const div = document.createElement('div')
-    div.textContent = `${response.count ? response.count : 'ZERO'} bears found`
+    div.textContent = `${url}: BEAR COUNT: ${bg.bears[url]}`
     document.body.appendChild(div)
-  }
+  })
+
+
+//   document.querySelector('button').addEventListener('click',
+//     onclick, false)
+
+//     function onclick() {
+//       chrome.tabs.query({ currentWindow: true, active: true },
+//         function (tabs) {
+//           chrome.tabs.sendMessage(tabs[0].id, "Hi", setCount)
+//         }
+//       )
+//     }
+
+//   function setCount(response) {
+//     const div = document.createElement('div')
+//     div.textContent = `${response.count ? response.count : 'ZERO'} bears found`
+//     document.body.appendChild(div)
+//   }
 }, false)
